f44088ef6bd5dc51558ba171ed5b1b60ab291bfe
Cola Chan <greenzorromail@gmail.com>
2018-02-09 18:01:49 +0800

    no message

--- app/functions.js
+++ app/functions.js
@@ -3,9 +3,3 @@ module.exports.globalFunc = globalFunc
 function globalFunc() {
 	console.log(getCurrentPages())  //显示页面路由
 }
-
-// 点击键盘下一步，焦点移至下一个表单
-module.exports.focusMove = focusMove
-function focusMove(e) {
-	// console.log(parseInt(e.currentTarget.dataset.moveSequency) + 1)
-}
--- app/pages/profile/profile.js
+++ app/pages/profile/profile.js
@@ -4,11 +4,8 @@ var appFunc = require('../../functions.js')
 Page({
 
 	data: {
-		focus1: false,
-		focus2: false,
-		focus3: false,
-		confirmType1: 'next',
-		confirmType2: 'next'
+		focusStatus: [true, false, false, false, false, false],
+		confirmType: ['next', 'next', 'next', 'next', 'next', 'done']
 	},
 	onUnload: function () {
 		console.log('页面卸载，此时保存并提交表单')
@@ -16,45 +13,36 @@ Page({
 	onShow: function () {
 		appFunc.globalFunc()  // 运行全局逻辑
 	},
-	// 输入框获得焦点，根据有无内容决定键盘右下角按钮文字
-	focusSetting1: function (e) {
-		if (e.detail.value.length <= 0) {
-			this.setData({
-				confirmType1: 'next'
-			})
+	// 根据有无内容决定键盘右下角按钮文字
+	inputSetting: function (e) {
+		var thisSequency = parseInt(e.target.dataset.focusSequency) || 0,
+			typeArray = this.data.confirmType
+		if (e.detail.value.length <= 0 && thisSequency < typeArray.length - 1) {
+			typeArray[thisSequency] = 'next'
 		}
 		else {
-			this.setData({
-				confirmType1: 'done'
-			})
+			typeArray[thisSequency] = 'done'
 		}
-	},
-	focusSetting2: function (e) {
-		if (e.detail.value.length <= 0) {
-			this.setData({
-				confirmType2: 'next'
-			})
-		}
-		else {
-			this.setData({
-				confirmType2: 'done'
-			})
-		}
+		this.setData({
+			confirmType: typeArray
+		})
+		console.log(typeArray)  //debug
 	},
 	// 点击键盘下一步，焦点移至下一个表单
-	focusMove1: function (e) {
-		if (this.data.confirmType1 == 'next') {
-			this.setData({
-				focus2: true
-			})
+	focusMove: function (e) {
+		var i, len, thisSequency = parseInt(e.target.dataset.focusSequency) || 0,
+			typeArray = this.data.confirmType,
+			statusArray = this.data.focusStatus
+		for (i = 0, len = statusArray.length; i < len; i++) {
+			statusArray[i] = false
+			if (i == thisSequency + 1 && typeArray[thisSequency] == 'next') {
+				statusArray[i] = true
+			}
 		}
-	},
-	focusMove2: function (e) {
-		if (this.data.confirmType2 == 'next') {
-			this.setData({
-				focus3: true
-			})
-		}
+		this.setData({
+			focusStatus: statusArray
+		})
+		console.log(statusArray)  //debug
 	}
 
 })
